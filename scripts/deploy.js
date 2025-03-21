751eb85866b79bea8ecdb309bdcd5a2beeb4a2ed
piusezekiel <e.pius1@alustudent.com>
2025-03-21 11:35:52 +0200

    initialize token transfer dapp with Hardhat and OpenZeppelin ERC20 token

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Token = await hre.ethers.getContractFactory("Token");
+  const initialSupply = 1000000;
+  const token = await Token.deploy(initialSupply);
+
+  await token.deployed();
+  console.log(`Token deployed to: ${token.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
